e7de5d57708bc358dc00dbf9f5b9ad999fc8e26b
DESKTOP-M55NDJS\arthu <arthurspring2@gmail.com>
2023-02-21 11:27:04 -0300

    Implemented Raycast and added comments to script.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI; // Quando usar o NavMeshAgent.
+using UnityEngine.AI; // Use this when using NavMeshAgent.
 
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    Ray lastRay; 
 
     void Update()
     {
+        if(Input.GetMouseButtonDown(0)) // Check if the left mouse button was clicked.
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition); //Cast a ray from the mouse position using the main camera.
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100); // Draw the ray for debugging purposes.
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
